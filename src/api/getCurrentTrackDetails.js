0d8debd170c136159c9b9734ab00e72642c0ad38
DiarCode <diar_beg64@mail.ru>
2022-08-15 20:17:23 +0600

    refactor files name

--- src/api/getCurrentTrackDetails.js
+++
@@ -1,9 +0,0 @@
-export const getCurrentSongDetails = async ({ currentSongId, accessToken }) => {
-  if (!currentSongId && !accessToken) {
-    return {};
-  }
-
-  return await fetch(`https://api.spotify.com/v1/tracks/${currentSongId}`, {
-    headers: { Authorization: `Bearer ${accessToken}` },
-  }).then(res => res.json());
-};
